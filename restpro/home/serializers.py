f6ace951f00df4314cab9e1f46822503c5900b68
harthikkumar <techharthik@gmail.com>
2025-10-04 21:32:31 +0530

    First commit

---
+++ restpro/home/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from home.models import Company
+#create serializers here
+
+class companySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        models = Company
+        fields ="__all__"
